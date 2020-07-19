efe63c073bbf3e9b034d61ba7607055cc7c7f70e
ericwm76 <eric.meldrum@gmail.com>
2020-07-19 14:57:58 -0600

    Move all assets into src/assets folder; create pull request template

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import './Nav.css'
+import React from 'react';
+import './Nav.css';
+import '../../assets/logo.png'
 
 const Nav = () => {
 
 
   return (
     <div className="nav">
-      <img src="../../logo.png" alt="Intueat logo"/>
+      <img src="../../assets/logo.png" alt="Intueat logo"/>
       <div className="signin-btns">
         <button>Sign In</button>
         <button>Create Account</button>
