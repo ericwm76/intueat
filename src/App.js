0b6b5d1707a68a46d6e220293afc11085ebfcdad
ericwm76 <eric.meldrum@gmail.com>
2020-07-19 13:54:35 -0600

    Render chef details in Left Side component

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ const App = () => {
   const [chefData, setChefData] = useState(data);
   const [isMobile, setIsMobile] = useState(false);
 
-  
 
   return (
     <div className="App">
--- src/components/Left/Left.js
+++ src/components/Left/Left.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import './Left.css'
 
-const LeftSideBar = (props) => {
-  const {chefData} = props;
-const credentials = chefData.experience.map(credential => <li>{credential}</li>)
-  
+const LeftSideBar = ({chefData}) => {
+  const tags = chefData.tags.map(tag => <li>{tag}</li>);
+  const credentials = chefData.experience.map(credential => <li>{credential}</li>);
+  const bookingRequirements = chefData.requirements.map(requirement => <li>{requirement}</li>);
+  const mealIncludes = chefData.includes.map(includedItem => <li>{includedItem}</li>);
+  const {reviews} = chefData;
+  const reviewAvg = () => {
+    const reviewsSum = reviews.reduce((acc, review) => {
+      acc += review.rating;
+      console.log(acc)
+      return acc
+    }, 0);
+    return reviewsSum / reviews.length;
+  };
+
   return (
     <div className="left-sidebar">
       <div className="chef-details">
         <h2>{chefData.name}</h2>
         <p>{chefData.accolade}</p>
         <p>{chefData.location}</p>
+        <ul>
+          {tags}
+        </ul>
       </div>
       <div className="chef-details">
         <h2>About the Chef</h2>
@@ -21,6 +35,23 @@ const credentials = chefData.experience.map(credential => <li>{credential}</li>)
         <ul>
           {credentials}
         </ul>
+      </div>
+      <div className="chef-details">
+        <h2>Booking Requirements</h2>
+        <ul>
+          {bookingRequirements}
+        </ul>
+      </div>
+      <div className="chef-details">
+        <h2>Meal Includes</h2>
+        <ul>
+          {mealIncludes}
+        </ul>
+      </div>
+      <div className="chef-details">
+        <h2>Ratings & Reviews</h2>
+        <p>{reviewAvg()}</p>
+        <p>{reviews.length}</p>
       </div>
     </div>
   )
