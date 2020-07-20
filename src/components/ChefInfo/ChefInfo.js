642498b7040005308b862142240d45c3c763d503
ericwm76 <eric.meldrum@gmail.com>
2020-07-20 09:48:11 -0600

    Create ReviewAvg component; cause it to conditionally render in ChefInfo based on isMobile property

--- src/components/ChefInfo/ChefInfo.js
+++ src/components/ChefInfo/ChefInfo.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import './ChefInfo.css'
+import ReviewAvg from '../ReviewAvg/ReviewAvg';
+import './ChefInfo.css';
 
-const ChefInfo = ({chefData}) => {
+const ChefInfo = ({chefData, isMobile}) => {
   const tags = chefData.tags.map(tag => <li className="tag">{tag}</li>);
   const credentials = chefData.experience.map(credential => <li>{credential}</li>);
   const bookingRequirements = chefData.requirements.map(requirement => <li>{requirement}</li>);
   const mealIncludes = chefData.includes.map(includedItem => <li>{includedItem}</li>);
   const {reviews} = chefData;
-  const findReviewAvg = () => {
-    const reviewsSum = reviews.reduce((acc, review) => {
-      acc += review.rating;
-      return acc;
-    }, 0);
-    return reviewsSum / reviews.length;
-  };
   const getReviews = () => {
     const reviewElems = reviews.map(review => {
       const firstName = review.name.split(' ')[0];
@@ -31,9 +25,12 @@ const ChefInfo = ({chefData}) => {
   }
 
   return (
-    <div className="chef-info">
+    <div className={isMobile?"chef-info chef-info-mobile":"chef-info"}>
       <div className="chef-details">
-        <h1>{chefData.name}</h1>
+        <div className="chef-title">
+          <h1 className={isMobile && "chef-name-mobile"}>{chefData.name}</h1>
+          {isMobile && <ReviewAvg reviews={reviews} />}
+        </div>
         <p>{chefData.accolade} • {chefData.location}</p>
         <ul className="tags-container">
           {tags}
@@ -64,8 +61,7 @@ const ChefInfo = ({chefData}) => {
       <div className="chef-details">
         <h2>Ratings & Reviews</h2>
         <div className="reviews">
-          <img className="large-star" src={require("../../assets/star.png")} alt="Orange star"/>
-          <h1>{findReviewAvg()}</h1>
+          <ReviewAvg reviews={reviews} />
           <p>{reviews.length} reviews</p>
         </div>
       </div>
