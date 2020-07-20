50fc9113d71b6abb06bbd0dc35aafa2a77b1d16f
ericwm76 <eric.meldrum@gmail.com>
2020-07-20 10:16:16 -0600

    Style Reservations card for mobile and cause it to conditionally render child elements based on isMobile property

--- src/components/Reservations/Reservations.js
+++ src/components/Reservations/Reservations.js
@@ -1,49 +1,56 @@
 import React, {useState} from 'react';
+import ReviewAvg from '../ReviewAvg/ReviewAvg';
 import './Reservations.css';
 
-const Reservations = ({chefData}) => {
-  const [count, setCount] = useState(0)
-  const {reviews} = chefData;
-  const findReviewAvg = () => {
-    const reviewsSum = reviews.reduce((acc, review) => {
-      acc += review.rating;
-      return acc;
-    }, 0);
-    return reviewsSum / reviews.length;
-  };
-
-  return (
-    <div className="reservations-card">
-      <div className="resy-header">
-        <p className="price-per-person"><span className="price">${chefData.pricePerPerson}</span> per person</p>
-        <div className="reviews">
-          <img className="large-star" src={require("../../assets/star.png")} alt="Orange star"/>
-          <h2 className="review-avg">{findReviewAvg()}</h2>
-        </div>
-      </div>
-      <div className="resy-inputs">
-        <div className="resy-input">
-          <p className="resy-input-label" for="date-input">DATE</p>
-          <input type="date"/>
-        </div>
-        <div className="resy-input">
-          <p className="resy-input-label" for="address-input">Location</p>
-          <input type="text"/>
-        </div>
-        <div className="resy-input">
-          <div className="counter">
-            <p>Headcount</p>
-            <div className="counter-btns">
-              <button className="counter-btn">-</button>
-              <p>{count}</p>
-              <button className="counter-btn">+</button>
-            </div>
+const Reservations = ({chefData, isMobile}) => {
+  const [count, setCount] = useState(0);
+  const selectCardStyle = () => {
+    if (isMobile) {
+      return (
+        <div className="reservations-card reservations-card-mobile">
+          <div className="resy-header">
+            <p className="price-per-person"><span className="price">${chefData.pricePerPerson}</span> per person</p>
+            <ReviewAvg reviews={chefData.reviews} />
+          <button className="reserve-btn">Reserve and Pay Deposit</button>
           </div>
         </div>
-      </div>
-      <button className="reserve-btn">Reserve and Pay Deposit</button>
-      <p className="terms-and-conds-text">By continuing you agree to our <a>Terms & Conditions</a></p>
-    </div>
+      )
+    } else {
+      return (
+        <div className="reservations-card">
+          <div className="resy-header">
+            <p className="price-per-person"><span className="price">${chefData.pricePerPerson}</span> per person</p>
+            <ReviewAvg reviews={chefData.reviews} />
+          </div>
+          <div className="resy-inputs">
+            <div className="resy-input">
+              <p className="resy-input-label" for="date-input">DATE</p>
+              <input type="date"/>
+            </div>
+            <div className="resy-input">
+              <p className="resy-input-label" for="address-input">Location</p>
+              <input type="text"/>
+            </div>
+            <div className="resy-input">
+              <div className="counter">
+                <p>Headcount</p>
+                <div className="counter-btns">
+                  <button className="counter-btn">-</button>
+                  <p>{count}</p>
+                  <button className="counter-btn">+</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <button className="reserve-btn">Reserve and Pay Deposit</button>
+          <p className="terms-and-conds-text">By continuing you agree to our <a>Terms & Conditions</a></p>
+        </div>
+      )
+    }
+  }
+
+  return (
+    selectCardStyle()
   )
 }
 
